f41f536b3fd4de2a83918b24b5cfdaad05035219
Alifhy01 <150313317+LIFHY@users.noreply.github.com>
2024-05-16 13:43:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 60)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
